ccc8bb4d2676bef67f433bafd71bf714963d65d6
raghavi <raghavi92@gmail.com>
2024-09-11 12:33:51 +0530

    update utils functions

--- app/Utils.java
+++ app/Utils.java
@@ -1,7 +1,14 @@
 public class Utils {
     public static void printArray(int[] nums) {
         for (int i : nums) {
-            System.out.println(i);
+            System.out.print(i);
+        }
+    }
+
+    public static void print2dArray(int[][] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            printArray(nums[i]);
+            System.out.println();
         }
     }
 }
